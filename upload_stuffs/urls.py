f27ff2add2968ed8a688193f264fc563cdb17320
prachikalbhor <62927856+prachikalbhor@users.noreply.github.com>
2020-04-14 11:48:34 +0530

    Add files via upload

---
+++ upload_stuffs/urls.py
@@ -0,0 +1,30 @@
+"""upload_stuffs URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from uploads import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.index,name='home'),
+    path('register/', views.register,name='register'),
+    path('login/', views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('upload/', views.upload,name='upload'),
+    path('file/',views.files,name='file'),
+    url(r'^delete1/(?P<file_id>[0-9]+)/$',views.delete1,name="delete"),
+]
